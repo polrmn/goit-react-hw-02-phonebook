d3a719ac6608d5877b00e3e9e344f7dad41e585a
polrmn <polrmn@gmail.com>
2023-02-02 15:02:08 +0200

    step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,90 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from "nanoid";
+import { Component } from "react";
+
+export class App extends Component {
+  nameInputId = nanoid();
+  numberInputId = nanoid();
+  filterInputId = nanoid();
+
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  handleInputChange = event => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  };
+  handleFormSubmit = event => {
+    event.preventDefault();
+    const newContact = {
+      name: this.state.name,
+      number: this.state.number,
+      id: nanoid(),
+    };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+      name: '',
+      number: '',
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleFormSubmit}>
+          <label htmlFor={this.nameInputId}>Name</label>
+          <input
+            onChange={this.handleInputChange}
+            value={this.state.name}
+            id={this.nameInputId}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <label htmlFor={this.numberInputId}>Number</label>
+          <input
+            onChange={this.handleInputChange}
+            value={this.state.number}
+            id={this.numberInputId}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+
+          <button type="submit">Add contact</button>
+        </form>
+        {this.state.contacts.length > 0 && <h1>Contacts</h1>}
+        <label htmlFor={this.filterInputId}>Find contacts by name</label>
+        <input
+          onChange={this.handleInputChange}
+          value={this.state.filter}
+          id={this.filterInputId}
+          type="text"
+          name="filter"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <ul>
+          {this.state.contacts.map(contact => (
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
